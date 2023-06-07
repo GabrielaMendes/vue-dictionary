07a7f1e6cf0d8034c40306f4df7d0195b00dd3f3
Gabriela <gdemelo.mendes@gmail.com>
2023-06-07 20:06:25 -0300

    layout: created app bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,56 @@
-<template>
+<script setup>
+const fontOptions = ["serif", "sans serif", "monospace"];
+</script>
 
+<template>
+	<v-app>
+		<!-- App Bar -->
+		<v-app-bar :elevation="0">
+			<template v-slot:prepend>
+				<v-icon color="primary" size="x-large" class="material-symbols-outlined"
+					>menu_book</v-icon
+				>
+			</template>
+			<v-app-bar-title class="font-weight-medium">
+        <span class="logo">VueDictionary</span>
+      </v-app-bar-title>
+
+			<v-spacer></v-spacer>
+
+			<v-menu open-on-hover>
+				<template v-slot:activator="{ props }">
+					<v-btn v-bind="props" class="text-capitalize text-body-1">
+						serif
+						<v-icon size="x-large" color="primary">expand_more</v-icon>
+					</v-btn>
+				</template>
+
+				<v-list>
+					<v-list-item v-for="font in fontOptions" :key="font" variant="plain">
+						<v-list-item-title :value="font" class="text-capitalize">{{
+							font
+						}}</v-list-item-title>
+					</v-list-item>
+				</v-list>
+			</v-menu>
+
+			<v-divider :vertical="true" :inset="true" class="ml-2"></v-divider>
+
+			<v-btn icon class="ml-2">
+				<v-icon size="x-large" class="material-symbols-outlined"
+					>dark_mode</v-icon
+				>
+			</v-btn>
+		</v-app-bar>
+	</v-app>
 </template>
 
-<script>
+<style>
+.material-symbols-outlined {
+	font-variation-settings: "FILL" 0, "wght" 400, "GRAD" 0, "opsz" 48;
+}
 
-</script>
+.logo {
+	letter-spacing: 1px;
+}
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,27 @@
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import { aliases, md } from 'vuetify/iconsets/md'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+	icons: {
+		defaultSet: "md",
+		aliases,
+		sets: {
+			md,
+		},
+	},
+	theme: {
+		themes: {
+			light: {
+				dark: false,
+				colors: {
+					primary: "#009688",
+					secondary: "#B2DFDB",
+				},
+			},
+		},
+	},
+});
