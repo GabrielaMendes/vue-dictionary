df0e70861646e29999bef971e57d9bdfd83fd6b7
Gabriela <gdemelo.mendes@gmail.com>
2023-06-08 17:04:33 -0300

    layout: created word meanings section

--- src/App.vue
+++ src/App.vue
@@ -71,6 +71,61 @@ const fontOptions = ["serif", "sans serif", "monospace"];
 					<v-icon color="primary" size="40">play_arrow</v-icon>
 				</v-btn>
 			</div>
+
+			<!-- Meanings -->
+				<div class="mt-10">
+          <h2 class="text-h5 font-weight-bold font-italic">noun</h2>
+          <v-divider></v-divider>
+          <h3 class="mt-6 ml-6 text-h5 text-blue-grey-lighten-1">Meaning</h3>
+          <v-list class="ml-4 mt-1">
+            <v-list-item>
+              <template v-slot:prepend>
+                <v-icon color="primary" size="small">arrow_forward</v-icon>
+              </template>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum,
+              quisquam eaque. Optio magni ratione, enim necessitatibus repellat
+              placeat vel alias, nihil aut voluptatum at sequi impedit incidunt
+              perspiciatis deserunt aliquam.
+            </v-list-item>
+            <v-list-item>
+              <template v-slot:prepend>
+                <v-icon color="primary" size="small">arrow_forward</v-icon>
+              </template>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum,
+              quisquam eaque. Optio magni ratione, enim necessitatibus repellat
+              placeat vel alias, nihil aut voluptatum at sequi impedit incidunt
+              perspiciatis deserunt aliquam.
+            </v-list-item>
+            <v-list-item>
+              <template v-slot:prepend>
+                <v-icon color="primary" size="small">arrow_forward</v-icon>
+              </template>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+              magnam.
+            </v-list-item>
+          </v-list>
+          <div class="mt-6 ml-6 d-flex">
+            <h3 class="text-h5 text-blue-grey-lighten-1 mr-6">Synonyms</h3>
+            <p class="text-h5 font-weight-medium text-primary">
+              eletronic keyborad
+            </p>
+          </div>
+        </div>
+
+        <div class="mt-10">
+          <h2 class="text-h5 font-weight-bold font-italic">verb</h2>
+          <v-divider></v-divider>
+          <h3 class="mt-6 ml-6 text-h5 text-blue-grey-lighten-1">Meaning</h3>
+          <v-list class="ml-4 mt-1">
+            <v-list-item>
+              <template v-slot:prepend>
+                <v-icon color="primary" size="small">arrow_forward</v-icon>
+              </template>
+              <v-list-item-title>To type on a computer keyboard</v-list-item-title>
+              <v-list-item-subtitle class="mt-1 text-body-1">"Keyboarding is the part of this job I like the most."</v-list-item-subtitle>
+            </v-list-item>
+          </v-list>
+        </div>
 		</v-container>
 	</v-app>
 </template>
