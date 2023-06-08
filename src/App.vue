03847cfe2f16829f3eafec98e0f6670c91945dee
Gabriela <gdemelo.mendes@gmail.com>
2023-06-08 18:07:45 -0300

    layout change: changed searchbar icon size

--- src/App.vue
+++ src/App.vue
@@ -63,7 +63,7 @@ const darkMode = ref(false)
 			>
 				<template v-slot:append-inner>
 					<v-btn icon elevation="0" color="transparent" class="mr-n2">
-						<v-icon size="x-large" color="primary" class="mr-1">search</v-icon>
+						<v-icon size="large" color="primary" class="mr-1">search</v-icon>
 					</v-btn>
 				</template>
 			</v-text-field>
