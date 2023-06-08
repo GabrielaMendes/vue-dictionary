d48d4d5a29c73add13ccfc7be173a1d7860aa569
Gabriela <gdemelo.mendes@gmail.com>
2023-06-08 17:35:10 -0300

    layout: created footer

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,10 @@ const fontOptions = ["serif", "sans serif", "monospace"];
 			<v-app-bar-title class="font-weight-medium">
 				<span class="logo">VueDictionary</span>
 			</v-app-bar-title>
-			
-      <v-spacer></v-spacer>
-			
-      <v-menu open-on-hover>
+
+			<v-spacer></v-spacer>
+
+			<v-menu open-on-hover>
 				<template v-slot:activator="{ props }">
 					<v-btn v-bind="props" class="text-capitalize text-body-1">
 						serif
@@ -43,7 +43,7 @@ const fontOptions = ["serif", "sans serif", "monospace"];
 		<v-container class="mt-16">
 			<!-- Search Bar -->
 			<v-text-field
-        class="mt-4"
+				class="mt-4"
 				variant="filled"
 				:autofocus="true"
 				:rules="[]"
@@ -73,59 +73,72 @@ const fontOptions = ["serif", "sans serif", "monospace"];
 			</div>
 
 			<!-- Meanings -->
-				<div class="mt-10">
-          <h2 class="text-h5 font-weight-bold font-italic">noun</h2>
-          <v-divider></v-divider>
-          <h3 class="mt-6 ml-6 text-h5 text-blue-grey-lighten-1">Meaning</h3>
-          <v-list class="ml-4 mt-1">
-            <v-list-item>
-              <template v-slot:prepend>
-                <v-icon color="primary" size="small">arrow_forward</v-icon>
-              </template>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum,
-              quisquam eaque. Optio magni ratione, enim necessitatibus repellat
-              placeat vel alias, nihil aut voluptatum at sequi impedit incidunt
-              perspiciatis deserunt aliquam.
-            </v-list-item>
-            <v-list-item>
-              <template v-slot:prepend>
-                <v-icon color="primary" size="small">arrow_forward</v-icon>
-              </template>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum,
-              quisquam eaque. Optio magni ratione, enim necessitatibus repellat
-              placeat vel alias, nihil aut voluptatum at sequi impedit incidunt
-              perspiciatis deserunt aliquam.
-            </v-list-item>
-            <v-list-item>
-              <template v-slot:prepend>
-                <v-icon color="primary" size="small">arrow_forward</v-icon>
-              </template>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
-              magnam.
-            </v-list-item>
-          </v-list>
-          <div class="mt-6 ml-6 d-flex">
-            <h3 class="text-h5 text-blue-grey-lighten-1 mr-6">Synonyms</h3>
-            <p class="text-h5 font-weight-medium text-primary">
-              eletronic keyborad
-            </p>
-          </div>
-        </div>
+			<div class="mt-10">
+				<h2 class="text-h5 font-weight-bold font-italic">noun</h2>
+				<v-divider></v-divider>
+				<h3 class="mt-6 ml-6 text-h5 text-blue-grey-lighten-1">Meaning</h3>
+				<v-list class="ml-4 mt-1">
+					<v-list-item>
+						<template v-slot:prepend>
+							<v-icon color="primary" size="small">arrow_forward</v-icon>
+						</template>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum,
+						quisquam eaque. Optio magni ratione, enim necessitatibus repellat
+						placeat vel alias, nihil aut voluptatum at sequi impedit incidunt
+						perspiciatis deserunt aliquam.
+					</v-list-item>
+					<v-list-item>
+						<template v-slot:prepend>
+							<v-icon color="primary" size="small">arrow_forward</v-icon>
+						</template>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum,
+						quisquam eaque. Optio magni ratione, enim necessitatibus repellat
+						placeat vel alias, nihil aut voluptatum at sequi impedit incidunt
+						perspiciatis deserunt aliquam.
+					</v-list-item>
+					<v-list-item>
+						<template v-slot:prepend>
+							<v-icon color="primary" size="small">arrow_forward</v-icon>
+						</template>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+						magnam.
+					</v-list-item>
+				</v-list>
+				<div class="mt-6 ml-6 d-flex">
+					<h3 class="text-h5 text-blue-grey-lighten-1 mr-6">Synonyms</h3>
+					<p class="text-h5 font-weight-medium text-primary">
+						eletronic keyborad
+					</p>
+				</div>
+			</div>
 
-        <div class="mt-10">
-          <h2 class="text-h5 font-weight-bold font-italic">verb</h2>
-          <v-divider></v-divider>
-          <h3 class="mt-6 ml-6 text-h5 text-blue-grey-lighten-1">Meaning</h3>
-          <v-list class="ml-4 mt-1">
-            <v-list-item>
-              <template v-slot:prepend>
-                <v-icon color="primary" size="small">arrow_forward</v-icon>
-              </template>
-              <v-list-item-title>To type on a computer keyboard</v-list-item-title>
-              <v-list-item-subtitle class="mt-1 text-body-1">"Keyboarding is the part of this job I like the most."</v-list-item-subtitle>
-            </v-list-item>
-          </v-list>
-        </div>
+			<div class="mt-10">
+				<h2 class="text-h5 font-weight-bold font-italic">verb</h2>
+				<v-divider></v-divider>
+				<h3 class="mt-6 ml-6 text-h5 text-blue-grey-lighten-1">Meaning</h3>
+				<v-list class="ml-4 mt-1">
+					<v-list-item>
+						<template v-slot:prepend>
+							<v-icon color="primary" size="small">arrow_forward</v-icon>
+						</template>
+						<v-list-item-title
+							>To type on a computer keyboard</v-list-item-title
+						>
+						<v-list-item-subtitle class="mt-1 text-body-1"
+							>"Keyboarding is the part of this job I like the
+							most."</v-list-item-subtitle
+						>
+					</v-list-item>
+				</v-list>
+			</div>
+
+      <!-- Footer -->
+      <div class="mt-10 text-body-2">
+				<v-divider></v-divider>
+        <p class="mt-6 text-blue-grey-lighten-1">Powered by 
+          <a href="https://dictionaryapi.dev/" target="_blank" class="text-black ml-3">Dictionary API <v-icon color="blue-grey-lighten-1" size="medium" class="ml-1">open_in_new</v-icon></a>
+        </p>
+      </div>
 		</v-container>
 	</v-app>
 </template>
