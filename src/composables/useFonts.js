15fab3a280f298e0978246688aca476f48945b7d
Gabriela <gdemelo.mendes@gmail.com>
2023-06-14 14:43:56 -0300

    fix: fixed font name displayed on reload

--- src/composables/useFonts.js
+++ src/composables/useFonts.js
@@ -1,15 +1,16 @@
 import { ref } from "vue";
 
+const fonts = {
+  serif: "'Noto Serif', serif",
+  "sans serif": "'Poppins', sans-serif",
+  monospace: "'JetBrains Mono', Courier, monospace",
+};
+
+const currFont = ref("serif")
+
 export const useFonts = () => {
-	const fonts = {
-		serif: "'Noto Serif', serif",
-		"sans serif": "'Poppins', sans-serif",
-		monospace: "'JetBrains Mono', monospace",
-	};
-
-  const currFont = ref("serif")
-
   const root = document.querySelector(":root");
+  
   const setFont = (newFont) => {
     currFont.value = newFont;
     root.style.setProperty('--font-family', fonts[newFont]);
